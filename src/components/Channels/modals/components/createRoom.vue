1cb01b017e9cf20caf2d9929b688d2d474cd8851
ederaleng <ederandreslobo1998@gmail.com>
2020-05-08 13:06:22 -0500

    Adding responsive design and sizing to modals

--- src/components/Channels/modals/components/createRoom.vue
+++ src/components/Channels/modals/components/createRoom.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="shadow-lg modal-container bg-black-100 w-6/12 mx-auto rounded shadow-lg z-50 overflow-y-auto"
+    class="shadow-lg modal-container bg-black-100 w-9/12 md:w-6/12 lg:w-4/12 mx-auto rounded shadow-lg z-50 overflow-y-auto"
   >
     <div class="modal-content text-left py-4 px-6">
       <div
         class="flex justify-between  items-center pb-3 text-hive-black border-b-2"
       >
-        <p class="text-2xl font-bold flex text-white">
+        <p class="text-lg lg:text-2xl font-bold flex text-white">
           <span> Create room </span>
         </p>
         <div class="modal-close z-50" @click="closeModal()">
@@ -25,7 +25,14 @@
       </div>
       <div class="my-2">
         <div class="py-4 px-2">
-          create room
+          <input
+            type="text"
+            class="w-full my-2 text-sm bg-grey-light text-grey-darkest rounded-md p-3 focus:outline-none"
+            placeholder="Name channel"
+          />
+          <button data-v-70c98a68="" class="bg-hive-red w-full h-10 my-3 lg:my-2 text-white font-bold py-2 px-4 rounded outline-none">
+            Create room
+          </button>
         </div>
       </div>
     </div>
